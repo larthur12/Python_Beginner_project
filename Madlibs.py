3e4625d08191985b5e1768a5e0c68eb2c85bf897
Arthur <lvelen1209@gmail.com>
2022-05-15 17:25:44 +0800

    Create a Madlibs game.

---
+++ Madlibs.py
@@ -0,0 +1,16 @@
+adj1=input("input a Adjective:")
+foods1=input("input a Foods:")
+foods2=input("input a Foods:")
+saying=input("input Saying:")
+verb=input("input a verb:")
+noun=input('input a noun:')
+color=input("input a color:")
+ride=input("someing you would ride in:")
+animal=input('input a animal:')
+person=input('input a person:')
+
+madlib=f"Today I went to my favorite Taco Stand called the {adj1} {animal}. Unlike most food stands, they cook and prepare the food in a \
+{ride} while you {verb}. The best thing on the menu is the {color} {noun}. Instead of ground beef they fill the taco with {foods2}, cheese, \
+and top it off with a salsa made from {foods1}. If that doesn't make your mouth water, then it' just like {person} always says: {saying}"
+
+print(madlib)
