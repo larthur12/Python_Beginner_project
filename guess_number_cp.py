2595102861f0044aec9381ceddddbdbc3c933aaf
Arthur <lvelen1209@gmail.com>
2022-05-16 17:39:54 +0800

    Computer guess number.

--- guess_number_cp.py
+++ guess_number_cp.py
@@ -13,6 +13,27 @@ def guess(x):
     print(f'Yes!The answer is {ans}')
 
 
+def guess_computer(x):
+    cp_status=''
+    ans=int(input('input your answer:'))
+    high=x
+    low=1
+    while cp_status!='c':
+        if high != low:
+            guess_cp=random.randint(low,high)
+        else:
+            guess_cp-=1
+            
+        cp_status=input(f'the answer {guess_cp} is correct(c) , lowwer(l) or higher(h)').lower()
+        if cp_status=='h':
+            high =guess_cp-1
+        elif cp_status=='l':
+            low=guess_cp+1
+    print('Computer have guessed the number correctly')
+
+        
+
 
 guess_range=int(input("input a guess range:"))
-guess(guess_range)
+# guess(guess_range)
+guess_computer(guess_range)
